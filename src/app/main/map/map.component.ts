f3fc383f0a6e7c60f3fd3e40953896d833ad2815
[stepsen89] <stephanie.senoner@gmail.com>
2019-05-18 02:48:08 +0100

    leaflet in typescript not displaying container

--- src/app/main/map/map.component.ts
+++ src/app/main/map/map.component.ts
@@ -10,16 +10,24 @@ import * as L from 'leaflet';
 export class MapComponent implements OnInit {  
   constructor() { }
 
-  options: any = {
+  // mymap: any = L.map('mapid').setView([51.505, -0.09], 13);
+  // data: any;
+      
+      ngOnInit() {
+  //       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  //         maxZoom: 18,
+  //         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+  //           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  //           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  //       }).addTo(this.mymap);
 
-    layers: [
-      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
-    zoom: 5,
-    center: L.latLng(46.879966, -121.726909)
-  }
-
-  ngOnInit() {
+  //       this.data = "test";
+        
+  //       L.marker([51.5308818372572, -0.122925992140273]).addTo(this.mymap)
+  //           .bindPopup("This is a mockup :)")
+  //           .openPopup();
+      
+  //       console.log(this.data);
   }
 
 }
