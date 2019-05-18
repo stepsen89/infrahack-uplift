e2ef5843bec13f6b25f40cbbbaad39ef9bb86be4
[stepsen89] <stephanie.senoner@gmail.com>
2019-05-18 13:24:16 +0100

    added user flow for single feedback

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ import { MainComponent } from './main/main.component';
 import { ListComponent } from './main/list/list.component';
 import { MapComponent } from './main/map/map.component';
 import { CommunityComponent } from './main/community/community.component';
+import { ReportingComponent } from './reporting/reporting.component';
+import { FormIncidenceComponent } from './reporting/form-incidence/form-incidence.component';
+import { FormInformationComponent } from './reporting/form-information/form-information.component';
 
 
 // main containing views for
@@ -29,7 +32,18 @@ const routes: Routes = [
         path: 'community',
         component: CommunityComponent
       }],
-  },
+  }, {
+    path: 'reporting',
+    component: ReportingComponent,
+    children: [{
+      path: 'incidence',
+      component: FormIncidenceComponent
+    }, 
+    {
+      path: 'details',
+      component: FormInformationComponent
+    }]
+  }
 ];
 
 @NgModule({
--- src/app/feedback.service.ts
+++ src/app/feedback.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const apiUrl = "http://192.168.8.100:8000/api/sent";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public getLocations(url?: string) {
+    return locationJSON;
+    this.httpClient.get(`${apiUrl}`)
+      .subscribe((res) => {
+        return res;
+      });
+  }
 }
+
+
