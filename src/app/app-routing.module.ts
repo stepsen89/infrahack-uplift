65e0123ec9fd1931340e69bd74bec5eccaa4b5f9
[stepsen89] <stephanie.senoner@gmail.com>
2019-05-18 01:05:10 +0100

    finish set up routing MVP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// set up routing for MVP
+
+import { MainComponent } from './main/main.component';
+import { ListComponent } from './main/list/list.component';
+import { MapComponent } from './main/map/map.component';
+import { CommunityComponent } from './main/community/community.component';
+
+const routes: Routes = [
+  { path: 'home',
+    component: MainComponent,
+    children: [{ 
+      path: 'stations',
+      component: ListComponent
+    },
+    { 
+      path: 'map',
+      component: MapComponent
+    },
+    { 
+      path: 'community',
+      component: CommunityComponent
+    }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
