b48788719c38a70faa14d3f92d1b834b245db208
[stepsen89] <stephanie.senoner@gmail.com>
2019-06-09 22:15:04 +0100

    make it more reusable with routing

--- src/app/support/steps-button/steps-button.component.ts
+++ src/app/support/steps-button/steps-button.component.ts
@@ -7,11 +7,17 @@ import { Component, OnInit, Input, AfterContentInit } from '@angular/core';
 })
 export class StepsButtonComponent implements OnInit, AfterContentInit {
   @Input() buttonTitle: string;
+  link: string;
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.buttonTitle)
+    console.log(this.buttonTitle);
+    if(this.buttonTitle === "Submit"){
+      this.link = "/home/map";
+    } else {
+      this.link = "/reporting/details"
+    }
   }
 
   ngAfterContentInit() {
