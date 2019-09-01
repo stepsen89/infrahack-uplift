c847fb1e72e2222c93e87920fe4426c44ce2fe2f
[stepsen89] <stephanie.senoner@gmail.com>
2019-09-01 23:22:59 +0100

    https

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 
-const uri = 'http://uplift.glitch.me/graphql';
+const uri = 'https://uplift.glitch.me/graphql';
 
 export function createApollo(httpLink: HttpLink) {
   return {
